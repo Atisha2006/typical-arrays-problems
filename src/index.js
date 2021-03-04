072bd3a4b77047ccef39b3974a3a62abd4fa81bd
Atisha2006 <atisha2006@mail.ru>
2021-03-04 23:52:42 +0300

    docs: correct test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  	return (array === undefined || array.length === 0)? 0 : Math.min.apply(null, array);	
 }
 
 exports.max = function max (array) {
-  return 0;
+	return (array === undefined || array.length === 0)? 0 : Math.max.apply(null, array);	
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let sum = 0;
+  	if (array == undefined || array.length == 0) {
+  		return 0;
+  	} else {
+		for (let item of array) { 		
+			sum += item;
+		}
+		return sum/array.length;
+	}	
 }
